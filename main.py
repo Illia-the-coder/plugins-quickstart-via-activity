ab73d056fc15b94086204aff098a414260c71db0
illia Liudohovskyi <101904816+Illia-the-coder@users.noreply.github.com>
2023-07-23 15:33:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,50 @@
 import json
-
 import quart
 import quart_cors
 from quart import request
+from github import Github, InputFileContent
+import logging
+import datetime
 
 app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
 
+# Define the Gist ID and filename
+GIST_ID = "YOUR_GIST_ID"
+FILENAME = "requests_log.csv"
+
+# Create a Github instance:
+g = Github("YOUR_GITHUB_TOKEN")
+
 # Keep track of todo's. Does not persist if Python session is restarted.
 _TODOS = {}
+
+def save_request_to_gist(request_type):
+    try:
+        now = datetime.datetime.now().isoformat()
+        row = ['"' + now + '"', '"' + request_type + '"']
+
+        # Get the Gist
+        gist = g.get_gist(GIST_ID)
+
+        # Get the current content of the Gist
+        current_content = gist.files[FILENAME].content
+
+        # Add the new row to the content
+        new_content = current_content + "\n" + ",".join(row)
+
+        # Update the Gist
+        try:
+            rate_limit_remaining = int(g.rate_limiting[0])
+        except ValueError:
+            rate_limit_remaining = 0
+
+        if rate_limit_remaining > 0:
+            gist.edit(files={FILENAME: InputFileContent(new_content)})
+            logging.info("Gist updated successfully.")
+        else:
+            logging.warning("Rate limit exceeded. Gist edit not performed.")
+    except Exception as e:
+        logging.error(f"Error while updating Gist: {e}")
 
 @app.post("/todos/<string:username>")
 async def add_todo(username):
@@ -15,10 +52,12 @@ async def add_todo(username):
     if username not in _TODOS:
         _TODOS[username] = []
     _TODOS[username].append(request["todo"])
+    save_request_to_gist("add_todo")
     return quart.Response(response='OK', status=200)
 
 @app.get("/todos/<string:username>")
 async def get_todos(username):
+    save_request_to_gist("get_todos")
     return quart.Response(response=json.dumps(_TODOS.get(username, [])), status=200)
 
 @app.delete("/todos/<string:username>")
@@ -28,6 +67,7 @@ async def delete_todo(username):
     # fail silently, it's a simple plugin
     if 0 <= todo_idx < len(_TODOS[username]):
         _TODOS[username].pop(todo_idx)
+    save_request_to_gist("delete_todo")
     return quart.Response(response='OK', status=200)
 
 @app.get("/logo.png")
